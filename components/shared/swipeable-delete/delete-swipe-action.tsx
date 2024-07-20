a81d28e67656d70df38c201bff0f2a7b03f911b7
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-07-21 00:59:01 +0200

    feat: class removing

--- components/shared/swipeable-delete/delete-swipe-action.tsx
+++ components/shared/swipeable-delete/delete-swipe-action.tsx
@@ -1,13 +1,15 @@
+import { cn } from "@/lib/utils";
 import { Pressable, Text } from "react-native";
 
-type DeleteSwipeActionProps = { removeTeacher: () => void };
+type DeleteSwipeActionProps = { removeTeacher: () => void; className?: string };
 export default function DeleteSwipeAction({
   removeTeacher,
+  className,
 }: DeleteSwipeActionProps) {
   return (
     <Pressable
       onPress={removeTeacher}
-      className="justify-center rounded-lg bg-vivid-red px-3"
+      className={cn("justify-center rounded-lg bg-vivid-red px-3", className)}
     >
       <Text className="text-lg text-white">Delete</Text>
     </Pressable>
